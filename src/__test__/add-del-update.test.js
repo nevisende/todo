aad860ea860bba6ad9f045bceea86fb6c2df967f
direakanbi <direakanbi@icloud.com>
2021-12-09 15:51:46 +0100

    Did testing for Update Description function

--- src/__test__/add-del-update.test.js
+++ src/__test__/add-del-update.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-import { updateDescription } from '../main.js';
+import { updateDescription, loopCheckBoxes } from '../main.js';
 
 jest.mock('../main', () => {
   const originalModule = jest.requireActual('../main');
@@ -15,12 +15,11 @@ jest.mock('../main', () => {
 
 describe('To-do test', () => {
   test('Should be edited with the updateDescription function', () => {
-
     document.body.innerHTML = ` <div id="todo">
          <span id="username" />
          <input type="text" value="Add To-do item for test" id="todo"/>
        <ul class="todo-list"><li class="">
-                <input type="checkbox" name="list">
+                <input type="checkbox" name="list" data-test="two">
                 <input type="text" name="input-list" data-test="one" value="asdas4" id="0">
                 <i class="fas fa-ellipsis-v"></i>
             </li><li class="">
@@ -50,4 +49,17 @@ describe('To-do test', () => {
     console.log(document.querySelector('#todo').querySelector('[data-test="one"]').value )
     expect(JSON.parse(localStorage.getItem('toDoListStorage'))[0].description).toBe('Changed');
   });
+
+  test('Should be updated with completed status', () => {
+    const storage = JSON.parse(localStorage.getItem('toDoListStorage')) ;
+    const checkbox = document.querySelector('[data-test="two"]');
+    loopCheckBoxes(storage);
+    let evt = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+    checkbox.dispatchEvent(evt);
+    expect(JSON.parse(localStorage.getItem('toDoListStorage'))[0].checked).toBe(true);
+  }) 
 });
